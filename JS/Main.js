06e6ceabebeccb5a9ca1fc0340828df95c43e2ae
Ahmed-eldaker <ahmeddaker01@gmail.com>
2024-11-09 01:22:05 +0200

    day1 js

---
+++ JS/Main.js
@@ -0,0 +1,100 @@
+// var test = {
+//   name: "mai",
+//   name: "mai",
+//   name: "mai",
+//   name: "mai"
+// };
+
+// x++;
+// console.log(x + y);
+// console.log(x);
+
+// console.log(x);
+// x += 2;
+// console.log(x);
+// x -= 2;
+// console.log(x);
+// x *= 2;
+// console.log(x);
+// x /= 2;
+// console.log(x);
+// x = 14 % 2;
+// console.log(x);
+
+// if (x == y) {
+//   console.log("x and y are equal");
+// } else if (x > y) {
+//   console.log("x greater than y");
+// } else {
+//   console.log("x smaller than y");
+// }
+
+// var color = "blue";
+
+// switch (color) {
+//   case "red":
+//     console.log("red");
+//     break;
+//   case "blue":
+//     console.log("blue");
+//     break;
+//   case "green":
+//     console.log("green");
+//     break;
+//   default:
+//     console.log("default");
+// }
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// for (var i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// console.log("i = " + i);
+// {
+
+// }
+// var y = 70;
+// var x = 1;
+// while (x <= 10) {
+//   console.log("!not equal 10");
+//   x++;
+// }
+// do {
+//   console.log("!not equal 10");
+//   x++;
+// } while (x >= 10);
+// console.log(5);
+// console.error("asfdgdh");
+// console.log(4);
+// var x = 5;
+// if (x == 5) {
+// var alertTest = alert("data ok");
+// }
+// console.log(alertTest);
+// var msg = confirm("are you ok?");
+// console.log(msg);
+// console.log(name);
+// var name = prompt("enter your name");
+// if (name ) {
+//   console.log("valid data");
+// } else {
+//   console.log("invalid data");
+// }
+
+// do {
+//   var name = prompt("enter your name");
+// } while (!isFinite(name) || !name);
+// console.log(name);
+
+// var test = 5;
+// console.log(isNaN("false"));
+// var msg =
+// console.log(parseInt("15.5"));
+// console.log(Number("2"));
+var name = 55;
+console.log(typeof Number(name));
+console.log(typeof undefined);
