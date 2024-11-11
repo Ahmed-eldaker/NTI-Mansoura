e7f688588a3e99aa5ae5ea402cf082ab42e7c939
Ahmed-eldaker <ahmeddaker01@gmail.com>
2024-11-11 17:59:53 +0200

    js-day2

--- JS/Main.js
+++ JS/Main.js
@@ -95,6 +95,118 @@
 // var msg =
 // console.log(parseInt("15.5"));
 // console.log(Number("2"));
-var name = 55;
-console.log(typeof Number(name));
-console.log(typeof undefined);
+// var name = 55;
+// console.log(typeof Number(name));
+// console.log(typeof undefined);
+
+// sum += 10;
+// console.log(sum);
+// var sum = 0;
+// do {
+//   var userInput = parseInt(prompt("enter num"));
+//   sum += userInput;
+// } while (userInput && sum < 100);
+// if (sum) {
+//   alert(sum);
+// } else {
+//   alert("not found");
+// }
+// // ---------------
+// var userName = prompt("enter your name");
+// if (userName && userName != "null") {
+//   var color = prompt("enter color (red-blue-green)");
+//   if (color && color != "null") {
+//     switch (color) {
+//       case "red":
+//         document.write("<h1 style='color:red;'> hello " + userName + " </h1>");
+//         break;
+//       case "blue":
+//         document.write("<h1 style='color:blue;'> hello " + userName + " </h1>");
+//         break;
+//       case "green":
+//         document.write(
+//           "<h1 style='color:green;'> hello " + userName + " </h1>"
+//         );
+//         break;
+
+//       default:
+//         document.write("<h1> hello " + userName + " </h1>");
+
+//         break;
+//     }
+//   } else {
+//     alert("invalid color");
+//   }
+// } else {
+//   alert("invalid name");
+// }
+// --------------------------
+
+// var userInput = prompt("enter your name");
+// if (userInput) {
+//   for (var i = 1; i <= 6; i++) {
+//     document.write("<h" + i + ">Hello " + userInput + " </h" + i + ">");
+//   }
+// } else {
+//   alert("pls enter your name");
+// }
+// ------------------
+// var test = new String("mai");
+// var test2 = "HELLO";
+// console.log(test2.split("", 3));\
+// ------------------
+// var emailReg = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+// var email = "nada@gmail.eg";
+// console.log(emailReg.test(email));
+
+// var phoneReg = /^01[0125][0-9]{8}$/;
+// var phone = "01011111111";
+// console.log(phoneReg.test(phone));
+
+// -------------------------------
+// var num = 5.6;
+// var num2 = new Number(15.656);
+// console.log(num2.valueOf(), num2.toString());
+// -------------------------------
+// var bool = new Boolean(null);
+// var bool2 = true;
+// console.log(bool.valueOf());
+
+// ------------------
+// var x = 5.1;
+// console.log(Math.round(Math.random() * 10));
+// -------------------
+// var date = new Date(2002, 4, 24);
+// var day = ["sun", "mon", "fri", "sat"];
+// console.log(
+// var day = date.toString();
+// console.log(day.split(" ")[0]);
+
+// );
+// console.log(typeof NaN);
+
+// var x = 5;
+// if (x == 5) {
+//   console.log("ok");
+// } else {
+//   console.log("no");
+// }
+
+// x == 5 ? console.log("ok") : console.log("no");
+// ----------------------
+
+// var str = "alia";
+// for (i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+
+var arr = [2, 3, 4, 5];
+
+// var arr = [6, 3, 4];
+// var arr2 = [1, 0, 2, 10, 11];
+// arr[4] = 2;
+// console.log((arr[4] = 5));
+// console.log(arr);
+// console.log(arr.sort((acc, nxt) => nxt - acc));
+console.log(arr.includes());
+// console.log(arr.sort);
