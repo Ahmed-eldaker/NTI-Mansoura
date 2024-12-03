a5b16132c409c547b0f10158efee0c280eedc411
Ahmed-eldaker <ahmeddaker01@gmail.com>
2024-12-03 18:24:46 +0200

    day2 angular

--- Angular/firstProj/src/app/app.routes.ts
+++ Angular/firstProj/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { GallaryComponent } from './components/gallary/gallary.component';
+// import { GalleyComponent } from './components/galley/galley.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path: '',redirectTo:"home" ,pathMatch:"full"},
+   {path: 'home',component:HomeComponent},
+   {path:'about',component:AboutComponent},
+   {path:'gallery',component:GallaryComponent}
+];
---
+++ Angular/firstProj/src/app/components/home/home.component.ts
@@ -0,0 +1,58 @@
+// import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { IProduct } from '../../Interfaces/iproduct';
+// import { IProduct } from '../../Interfaces/iproduct';
+// import { log } from 'console';
+
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+products: IProduct[]=[
+{
+  name: 'iphone',
+  price: 30000,
+  onSale: false,
+  imgSrc: './assets/imgs/Anastronautridingahorseinaphotorealisticstyle6.webp',
+  reviews:["Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet."]
+},{
+  name: 'samsung',
+  price: 25000,
+  onSale: false,
+  imgSrc: './assets/imgs/gratisography-cool-cat-800x525.jpg'
+}
+,{
+  name: 'oppo',
+  price: 12000,
+  onSale: false,
+  imgSrc: './assets/imgs/premium_photo-1664474619075-644dd191935f.jpg'
+},{
+  name: 'honor',
+  price: 5000,
+  onSale: true,
+  imgSrc: './assets/imgs/premium_photo-1664474619075-644dd191935f.jpg'
+}
+]
+auth:string="admin"
+  color="red"
+  isAdmin:boolean=false
+  isTrue:boolean=true
+  link:string="./assets/imgs/Anastronautridingahorseinaphotorealisticstyle6.webp"
+  name:string="jessica"
+  sayHello():void{
+  alert(`hello  ${this.name}`)
+    
+  }
+test(e:KeyboardEvent){
+  console.log(e.ctrlKey);
+  
+}
+}
